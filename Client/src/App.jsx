02db8063f8550c9b000eb0471b0fdc2714dd23b8
Varun Singh <varunsingh7004@gmail.com>
2025-05-21 19:32:12 +0530

    Right basic basic done

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -5,17 +5,17 @@ function App() {
   return (
     <div className="flex min-h-screen bg-black text-white gap-2">
       {/* Left side - 20% */}
-      <div className="w-[16%] border-r-1 border-white/15 p-2 pl-6  ">
+      <div className="w-[16%] border-r-1 border-white/15 p-2 pl-6">
         <LeftSide />
       </div>
 
       {/* Center - 60% */}
-      <div className="w-[58%]  border-r-1 border-white/15 p-2">
+      <div className="w-[54%]  border-r-1 border-white/15 p-2">
         Center Content
       </div>
 
       {/* Right side - 20% */}
-      <div className="w-[26%] p-2">
+      <div className="w-[30%] p-2">
         <RightSide />
       </div>
     </div>
--- Client/src/components/rightSide/RightSide.jsx
+++ Client/src/components/rightSide/RightSide.jsx
@@ -1,9 +1,38 @@
 import React from "react";
+import { Profile } from "../../assets";
 
 function RightSide() {
   return (
     <div>
-      <div>Right</div>
+      <div className="mt-8 flex items-center gap-3 text-xs">
+        <img
+          src={Profile}
+          className="rounded-full"
+          width="38px"
+          alt="Profile"
+        />
+        vadergotbaddies
+        <span className="ml-14 text-sky-500 ">Switch</span>
+      </div>
+      <div className="items-center text-xs mt-5 ">
+        <span className="font-semibold text-white/50">Suggested for you</span>
+        <span className="ml-27">See All</span>
+      </div>
+      <ul className="text-[11px] text-white/30 mt-5 flex flex-wrap gap-x-2 max-w-[240px]">
+        <li>About</li>
+        <li>Help</li>
+        <li>Press</li>
+        <li>API</li>
+        <li>Jobs</li>
+        <li>Privacy</li>
+        <li>Terms</li>
+        <li>Locations</li>
+        <li>Language</li>
+        <li>Meta Verified</li>
+      </ul>
+      <div className="text-[11px] text-white/30 mt-4">
+        © 2025 Instagram from Meta
+      </div>
     </div>
   );
 }
